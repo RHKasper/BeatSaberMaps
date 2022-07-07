9f424fb31ae970c364ea0883d060bfe870e49f51
Robert Kasper <98301621+RHKasper@users.noreply.github.com>
2022-07-07 01:36:48 -0400

    get spotify playlists for user

--- BeatSaberUnzipper/BeatSaberUnzipper/Program.cs
+++ BeatSaberUnzipper/BeatSaberUnzipper/Program.cs
@@ -5,6 +5,7 @@ using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Threading;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace BeatSaberUnzipper
@@ -17,73 +18,76 @@ namespace BeatSaberUnzipper
         private static readonly int[] PlaylistIds = {3210, 2363, 2364, 3209};
         private static int songsLeftToDownload = 0;
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
-            if(Directory.Exists(BeatsaberMapsFolder))
-                Directory.Delete(BeatsaberMapsFolder, true);
-            Directory.CreateDirectory(BeatsaberMapsFolder);
-            
-            if(Directory.Exists(PlaylistsFolderPath))
-                Directory.Delete(PlaylistsFolderPath, true);
-            Directory.CreateDirectory(PlaylistsFolderPath);
+            Console.WriteLine("Running Spotify Test");
+            await SpotifyTest.Test();
+            Console.WriteLine("Spotify Test Finished");
 
-            songsLeftToDownload = 0;
-
-            HashSet<string> mapIdsDownloaded = new HashSet<string>();
-            
-            foreach (int playlistId in PlaylistIds)
-            {
-                //download playlist file
-                BPList bpList = BeatSaverDownloader.GetBpList(playlistId, out string fileContents);
-                if (bpList == null)
-                {
-                    Console.WriteLine($"playlist {playlistId} could not be downloaded or read");
-                    continue;
-                }
-
-                // save playlist file contents
-                string path = Path.Combine(PlaylistsFolderPath, bpList.playlistTitle + ".bplist");
-                File.WriteAllText(path, fileContents);
-                Console.WriteLine("Saved Playlist: " + path);
-                Console.WriteLine("Queueing " + bpList.songs.Count + " for download...");
-                
-                // Download songs
-                foreach (Song song in bpList.songs)
-                {
-                    MapData mapData = BeatSaverDownloader.GetMapData(song);
-                    if (mapData == null)
-                    {
-                        Console.WriteLine($"Downloading {song.songName} song data failed");
-                        continue;
-                    }
-
-                    if (mapIdsDownloaded.Contains(mapData.id))
-                        continue;
-                    else
-                        mapIdsDownloaded.Add(mapData.id);
-                
-                    string zipFileName = mapData.name + " - " + mapData.id + ".zip";
-                    foreach (char c in Path.GetInvalidFileNameChars()) 
-                        zipFileName = zipFileName.Replace(c, ' ');
-                
-                    string zipFilePath = Path.Combine(BeatsaberMapsFolder, zipFileName);
-                    foreach (char c in Path.GetInvalidPathChars()) 
-                        zipFileName = zipFileName.Replace(c+"", "");
-
-                    songsLeftToDownload++;
-                    BeatSaverDownloader.DownloadZipFile(mapData.GetLatestVersion().downloadURL, zipFilePath, OnSongFinishedDownloading);
-                }
-            }
-
-            while (songsLeftToDownload != 0)
-            {
-                Console.WriteLine("Waiting for " + songsLeftToDownload + " songs to download");
-                Thread.Sleep(750);
-            }
-            
-            Console.WriteLine("Song and playlist download complete");
+            // if(Directory.Exists(BeatsaberMapsFolder))
+            //     Directory.Delete(BeatsaberMapsFolder, true);
+            // Directory.CreateDirectory(BeatsaberMapsFolder);
+            //
+            // if(Directory.Exists(PlaylistsFolderPath))
+            //     Directory.Delete(PlaylistsFolderPath, true);
+            // Directory.CreateDirectory(PlaylistsFolderPath);
+            //
+            // songsLeftToDownload = 0;
+            //
+            // HashSet<string> mapIdsDownloaded = new HashSet<string>();
+            //
+            // foreach (int playlistId in PlaylistIds)
+            // {
+            //     //download playlist file
+            //     BPList bpList = BeatSaverDownloader.GetBpList(playlistId, out string fileContents);
+            //     if (bpList == null)
+            //     {
+            //         Console.WriteLine($"playlist {playlistId} could not be downloaded or read");
+            //         continue;
+            //     }
+            //
+            //     // save playlist file contents
+            //     string path = Path.Combine(PlaylistsFolderPath, bpList.playlistTitle + ".bplist");
+            //     File.WriteAllText(path, fileContents);
+            //     Console.WriteLine("Saved Playlist: " + path);
+            //     Console.WriteLine("Queueing " + bpList.songs.Count + " for download...");
+            //     
+            //     // Download songs
+            //     foreach (Song song in bpList.songs)
+            //     {
+            //         MapData mapData = BeatSaverDownloader.GetMapData(song);
+            //         if (mapData == null)
+            //         {
+            //             Console.WriteLine($"Downloading {song.songName} song data failed");
+            //             continue;
+            //         }
+            //
+            //         if (mapIdsDownloaded.Contains(mapData.id))
+            //             continue;
+            //         else
+            //             mapIdsDownloaded.Add(mapData.id);
+            //     
+            //         string zipFileName = mapData.name + " - " + mapData.id + ".zip";
+            //         foreach (char c in Path.GetInvalidFileNameChars()) 
+            //             zipFileName = zipFileName.Replace(c, ' ');
+            //     
+            //         string zipFilePath = Path.Combine(BeatsaberMapsFolder, zipFileName);
+            //         foreach (char c in Path.GetInvalidPathChars()) 
+            //             zipFileName = zipFileName.Replace(c+"", "");
+            //
+            //         songsLeftToDownload++;
+            //         BeatSaverDownloader.DownloadZipFile(mapData.GetLatestVersion().downloadURL, zipFilePath, OnSongFinishedDownloading);
+            //     }
+            // }
+            //
+            // while (songsLeftToDownload != 0)
+            // {
+            //     Console.WriteLine("Waiting for " + songsLeftToDownload + " songs to download");
+            //     Thread.Sleep(750);
+            // }
+            //
+            // Console.WriteLine("Song and playlist download complete");
         }
 
         private static void OnSongFinishedDownloading(string path)
---
+++ BeatSaberUnzipper/BeatSaberUnzipper/SpotifyTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using SpotifyAPI.Web;
+
+namespace BeatSaberUnzipper
+{
+	class SpotifyTest
+	{ 
+		public static async Task Test()
+		{
+			var spotify = new SpotifyClient("BQBHec-CCCspN_LxH53wNOu9KVPUZ2uIYb06Vu0rkyNtw1vPG4G-jqRvaPDJ5ipSoTcVaial0DCREFi5mHWDnSd7Dxi7jOq8RpQtAHdLD11CnYjRv1BjUCL9wMZvYsbD08HTKm1TAQ61pbCG-HjhRNL7ZeJ2HsdHoWFoDa-UiwitFME");
+			FullTrack track = await spotify.Tracks.Get("0q7oMII7kWTj1ZSX6GT6LU");
+			Console.WriteLine(track.Name);
+			
+			PrivateUser user = await spotify.UserProfile.Current();
+
+			Paging<SimplePlaylist> playlists = await spotify.Playlists.GetUsers(user.Id);
+			foreach (var playlist in playlists.Items)
+			{
+				Console.WriteLine(playlist.Name);
+			}
+		}
+	}
+}
