ef01a48fcf45643e70faebbf57845e565d772af1
Robert Kasper <98301621+RHKasper@users.noreply.github.com>
2023-11-21 21:14:42 -0500

    Adds Halcyon12 mapper

--- BeatSaberUnzipper/BeatSaberUnzipper/Playlists/BeatSaverUserPlaylists.cs
+++ BeatSaberUnzipper/BeatSaberUnzipper/Playlists/BeatSaverUserPlaylists.cs
@@ -7,5 +7,6 @@ namespace BeatSaberUnzipper
         public const int ZaneSaber = 4284220;
         public const int TheCzar1994 = 4285984;
         public const int NixieKorten = 4286374; //electroswing
+        public const int Halcyon12 = 14808;
     }
 }
--- BeatSaberUnzipper/BeatSaberUnzipper/Program.cs
+++ BeatSaberUnzipper/BeatSaberUnzipper/Program.cs
@@ -24,6 +24,7 @@ namespace BeatSaberUnzipper
             BeatSaverUserPlaylists.Scarladore,
             BeatSaverUserPlaylists.ZaneSaber,
             BeatSaverUserPlaylists.TheCzar1994,
+            BeatSaverUserPlaylists.Halcyon12,
         };
         
         private static readonly string[] SpotifyPlaylistUrls =
