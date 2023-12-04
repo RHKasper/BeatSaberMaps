8a04724acd2d591e35e929c78b43e6e0553ac124
Robert Kasper <roberthkasper@gmail.com>
2023-12-03 20:49:43 -0500

    Updated playlists

--- BeatSaberUnzipper/BeatSaberUnzipper/Program.cs
+++ BeatSaberUnzipper/BeatSaberUnzipper/Program.cs
@@ -19,6 +19,9 @@ namespace BeatSaberUnzipper
             { "Aspirational", 7038 },
             { "Ajr NeoTheater", 171573 },
             { "Skillet", 85217 },
+            { "Linkin Park", 12144 },
+            { "KDA", 70553 },
+            { "BTS", 70543 },
         };
 
         private static readonly Dictionary<string, int> UserPlaylistIds = new()
@@ -26,17 +29,16 @@ namespace BeatSaberUnzipper
             {"Teuflum", 68740},
             {"TheCzar1994", 4285984},
             {"NixieKorten - Electroswing mapper ", 4286374},
-            {"Halcyon12", 14808},
             {"Revelate", 2768},
         };
         
         private static readonly Dictionary<string,string> SpotifyPlaylistUrls = new()
         {
-            {"All Likes", "https://open.spotify.com/playlist/5Zi1NzMK91ImLODGEWHNqS?si=bcde847d6766403f"},
             {"Robert's Epic Mix", "https://open.spotify.com/playlist/6QTGBDOzjxnkfQ9X6OmWIY?si=9d82829900c84462"},
             {"Robert's Pop Mix", "https://open.spotify.com/playlist/7vSZ2b4591qCZhi8bn2xkn?si=4a570e22013747fa"},
             {"Robert's Edm Mix", "https://open.spotify.com/playlist/5tY88XWKaUArUISNkY697j?si=3afb235dac594189"},
             {"Robert's Rock Mix", "https://open.spotify.com/playlist/43jMBPZVR5cdD7Cw1gZF8j?si=9a990d1647eb4cde"},
+            {"All Likes", "https://open.spotify.com/playlist/5Zi1NzMK91ImLODGEWHNqS?si=bcde847d6766403f"},
         };
         
         
